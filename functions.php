c12309111b9e14c8d50a166b753f784405ac2fa2
Aan Darmatirta <darmatirta@hotmail.com>
2022-08-26 03:18:03 +0700

    1st Commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Theme Customization
+ * 
+ * @package Kosongan
+ * @author  Darmatirta
+ * @license GPL-3.0-or-later
+ * @link    https://pedanha.com
+ */
+// load child theme textdomain
+load_child_theme_textdomain( 'kosongan' );
+
+add_action( 'genesis_setup', 'kosongan_setup' );
+/**
+ * Theme setup
+ * 
+ * Attach all of the site-wide functions to the correct hooks and filters. 
+ * All the functions themselves are defined below this setup functions
+ * 
+ * @since 1.0.0
+ */
+
+function kosongan_setup() {
+
+}
